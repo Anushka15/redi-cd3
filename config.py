e21e9ea374a0fedf4f941353fe3cbf35d2e59d95
Anushka15 <anushkakulkarni15@gmail.com>
2024-07-08 03:49:01 +0530

    comments

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # config.py
 
 DATABASE = {
-    'driver': 'postgresql',
-    'host': 'castle.ewi.utwente.nl',
-    'port': 5432,
-    'user': 'pp2324_11',
-    'password': 'R4exP/fv',
-    'dbname': 'pp2324_11'
+    'driver': '',
+    'host': '',
+    'port': '',
+    'user': '',
+    'password': '',
+    'dbname': ''
 }
